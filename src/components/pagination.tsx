eaf4e3138c2dcd31c5b7194758e8112c8af4de95
JoaoVittorL <vittorsantos234@gmail.com>
2024-09-29 13:11:15 -0300

    insert pagination component

---
+++ src/components/pagination.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button'
+import {
+  ChevronLeft,
+  ChevronRight,
+} from 'lucide-react'
+
+
+interface PaginationProps {
+  pageIndex: number
+  totalCount: number
+  perPage: number
+  onPageChange: (pageIndex: number) => Promise<void> | void
+}
+
+export function Pagination({
+  pageIndex,
+  onPageChange,
+}: PaginationProps) {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-6 lg:gap-8">
+        <div className="flex items-center gap-2">
+          <Button onClick={() => onPageChange(pageIndex - 1)} variant="default" className="h-8 w-8 p-0" disabled={pageIndex === 0}>
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Página anterior</span>
+          </Button>
+          <Button onClick={() => onPageChange(pageIndex + 1)} variant="default" className="h-8 w-8 p-0" >
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Próxima página</span>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/app/movimentation/index.tsx
+++ src/pages/app/movimentation/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button'
 import { Helmet } from 'react-helmet-async'
 
 export function Movimentation() {
@@ -7,9 +6,6 @@ export function Movimentation() {
       <Helmet title="Movimentation" />
       <div className='flex itens-center gap-2 justify-center'>
         <h1>Movimentation</h1>
-        <Button variant="default">Button</Button>
-        <Button variant="destructive">Button</Button>
-        <Button variant="ghost">Button</Button>
       </div>
     </>
   )
